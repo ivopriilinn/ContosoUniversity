cf3bdcb3f4bd4f3cdb42e6273702de87673b008f
Ivo Priilinn <ivopriilinn@gmail.com>
2023-09-29 01:37:59 +0300

    Changes to Student entity

--- Models/Student.cs
+++ Models/Student.cs
@@ -8,17 +8,27 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
         [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
         [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
-
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
